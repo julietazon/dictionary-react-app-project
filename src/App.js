26a379705168a3464b30ab48769c1ae7f7d876e0
julietazon <juubozon@gmail.com>
2021-03-24 00:09:11 -0300

    add flexbox structure

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <footer className="footer">
-        <small>
-          <span>This project is coded by JulietaZon and is </span>
-          <br />
-          and is{" "}
-          <span
-            className="Link"
-            onClick={() =>
-              window.open(
-                "https://github.com/julietazon/dictionary-react-app-project",
-                "_blank"
-              )
-            }
-          >
-            Open Source
-          </span>{" "}
-          on{" "}
-          <span
-            className="Link"
-            onClick={() => window.open("https://github.com/", "_blank")}
-          >
-            GitHub
-          </span>
-          ❣️
-        </small>
-      </footer>
+      <div className="wrapper">
+        <main className="main">
+          <header className="">
+            <h1>Dictionary</h1>
+          </header>
+          <Dictionary />
+        </main>
+        <aside className="aside aside-1"></aside>
+        <aside className="aside aside-2"></aside>
+        <footer className="footer">
+          <small>
+            <span>This project is coded by JulietaZon and is </span>
+            <br />
+            and is{" "}
+            <span
+              className="Link"
+              onClick={() =>
+                window.open(
+                  "https://github.com/julietazon/dictionary-react-app-project",
+                  "_blank"
+                )
+              }
+            >
+              Open Source
+            </span>{" "}
+            on{" "}
+            <span
+              className="Link"
+              onClick={() => window.open("https://github.com/", "_blank")}
+            >
+              GitHub
+            </span>
+            ❣️
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
