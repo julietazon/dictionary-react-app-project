3b20e45b3a6d6f8fb0c34737c8eb34ca71ea954c
julietazon <juubozon@gmail.com>
2021-03-24 00:42:51 -0300

    update fake components

---
+++ src/Images.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Images() {
+  return (
+    <div className="Images">
+      <ul>
+        <li>Image</li>
+        <li>Image</li>
+        <li>Image</li>
+        <li>Image</li>
+        <li>Image</li>
+        <li>Image</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/Word.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Word() {
+  return (
+    <div className="Word">
+      <h2>sunset</h2>
+      <h3>/ˈsənˌset/</h3>
+    </div>
+  );
+}
