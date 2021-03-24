3eb9221003839ebe59080074edcf63239404d4f2
julietazon <juubozon@gmail.com>
2021-03-24 01:44:21 -0300

    update phonetic

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,17 @@
 import React from "react";
+import audioIcon from "./img/audioiconblack.webp";
 
-export default function Phonetic() {
-  return <div className="Phonetic"></div>;
+export default function Phonetic(props) {
+  let audio = new Audio(props.phonetic.audio);
+
+  const start = () => {
+    audio.play();
+  };
+  return (
+    <div className="Phonetic">
+      <button className="btn PhoneticButton" onClick={start}>
+        <img src={audioIcon} />
+      </button>
+    </div>
+  );
 }
