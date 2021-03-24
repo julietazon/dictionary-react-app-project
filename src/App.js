487d7801e2e57f19d29947e7dfdd1a4546be9bcb
julietazon <juubozon@gmail.com>
2021-03-24 00:04:07 -0300

    add footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer className="footer">
+        <small>
+          <span>This project is coded by JulietaZon and is </span>
+          <br />
+          and is{" "}
+          <span
+            className="Link"
+            onClick={() =>
+              window.open(
+                "https://github.com/julietazon/dictionary-react-app-project",
+                "_blank"
+              )
+            }
+          >
+            Open Source
+          </span>{" "}
+          on{" "}
+          <span
+            className="Link"
+            onClick={() => window.open("https://github.com/", "_blank")}
+          >
+            GitHub
+          </span>
+          ❣️
+        </small>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
